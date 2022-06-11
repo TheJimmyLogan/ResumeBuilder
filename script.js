b99bca884af497612b173fac3485e1be3facc6d1
Эрик Э <rick-jimmy@mail.ru>
2022-06-11 21:41:17 +0300

    resume ficsed

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ let formData = {
 
 const loadFormData = () => {
     localStorageObject = JSON.parse(localStorage.getItem('formData'));
-    if(localStorageObject){
+    if (localStorageObject) {
         formData = localStorageObject;
         document.getElementById('firstName').value = localStorageObject?.generalInformation?.firstName || '';
         document.getElementById('lastName').value = localStorageObject?.generalInformation?.lastName || '';
@@ -38,49 +38,56 @@ window.addEventListener('resize', resizeVh);
 document.getElementById('generalInfoBtn').addEventListener('click', (e) => {
     document.querySelector('.active-step').classList.remove("active-step");
     e.target.classList.add('active-step');
-    document.getElementById('generalInfoSection').scrollIntoView({ behavior: 'smooth'});
+    document.getElementById('stepCount').innerText = '1';
+    document.getElementById('generalInfoSection').scrollIntoView({ behavior: 'smooth' });
 })
 
 window.addEventListener('resize', resizeVh);
 document.getElementById('personalInfoBtn').addEventListener('click', (e) => {
     document.querySelector('.active-step').classList.remove("active-step");
     e.target.classList.add('active-step');
-    document.getElementById('personalInfoSection').scrollIntoView({ behavior: 'smooth'});
+    document.getElementById('stepCount').innerText = '2';
+    document.getElementById('personalInfoSection').scrollIntoView({ behavior: 'smooth' });
 })
 
 window.addEventListener('resize', resizeVh);
 document.getElementById('experienceBtn').addEventListener('click', (e) => {
     document.querySelector('.active-step').classList.remove("active-step");
     e.target.classList.add('active-step');
-    document.getElementById('experienceSection').scrollIntoView({ behavior: 'smooth'});
+    document.getElementById('stepCount').innerText = '3';
+    document.getElementById('experienceSection').scrollIntoView({ behavior: 'smooth' });
 })
 
 window.addEventListener('resize', resizeVh);
 document.getElementById('eduBtn').addEventListener('click', (e) => {
     document.querySelector('.active-step').classList.remove("active-step");
     e.target.classList.add('active-step');
-    document.getElementById('eduSection').scrollIntoView({ behavior: 'smooth'});
+    document.getElementById('stepCount').innerText = '4';
+    document.getElementById('eduSection').scrollIntoView({ behavior: 'smooth' });
 })
 
 window.addEventListener('resize', resizeVh);
 document.getElementById('courseTraingBtn').addEventListener('click', (e) => {
     document.querySelector('.active-step').classList.remove("active-step");
     e.target.classList.add('active-step');
-    document.getElementById('courseTraingSection').scrollIntoView({ behavior: 'smooth'});
+    document.getElementById('stepCount').innerText = '5';
+    document.getElementById('courseTraingSection').scrollIntoView({ behavior: 'smooth' });
 })
 
 window.addEventListener('resize', resizeVh);
 document.getElementById('languageBtn').addEventListener('click', (e) => {
     document.querySelector('.active-step').classList.remove("active-step");
     e.target.classList.add('active-step');
-    document.getElementById('languageSection').scrollIntoView({ behavior: 'smooth'});
+    document.getElementById('stepCount').innerText = '6';
+    document.getElementById('languageSection').scrollIntoView({ behavior: 'smooth' });
 })
 
 window.addEventListener('resize', resizeVh);
 document.getElementById('additionalBtn').addEventListener('click', (e) => {
     document.querySelector('.active-step').classList.remove("active-step");
     e.target.classList.add('active-step');
-    document.getElementById('additionalSection').scrollIntoView({ behavior: 'smooth'});
+    document.getElementById('stepCount').innerText = '7';
+    document.getElementById('additionalSection').scrollIntoView({ behavior: 'smooth' });
 })
 
 
@@ -96,6 +103,29 @@ document.getElementById('lastName').addEventListener('input', (e) => {
 
 document.getElementById('fatherName').addEventListener('input', (e) => {
     formData.generalInformation.fatherName = e.target.value;
+})
+
+// Preview resume
+
+document.getElementById('preview').addEventListener('click', () => {
+    document.getElementById('formData').style.display = 'none';
+    document.getElementById('resumeContainer').style.display = 'block';
+    document.getElementById('back').style.display = 'block';
+    document.getElementById('preview').style.display = 'none';
+    document.getElementById('formSteps').style.display = 'none';
+    document.getElementById('resumeSteps').style.display = 'block';
+
+})
+
+// Go Back
+
+document.getElementById('back').addEventListener('click', () => {
+    document.getElementById('formData').style.display = 'block';
+    document.getElementById('resumeContainer').style.display = 'none';
+    document.getElementById('back').style.display = 'none';
+    document.getElementById('preview').style.display = 'block';
+    document.getElementById('formSteps').style.display = 'block';
+    document.getElementById('resumeSteps').style.display = 'none';
 })
 
 
